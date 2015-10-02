fec6ed38abc343f0ab712f2fb7fc38c5b5cb73f6
yotti <yotti@LEY1.GUILLEAUME.WERK>
2015-10-02 12:03:06 +0200

    Ich habe ein Paar Änderungen gemacht und noch dazu die methode addition implementiert

--- HelloWorld/src/Helloworld.java
+++ HelloWorld/src/Helloworld.java
@@ -2,11 +2,18 @@
 public class Helloworld {
 	public static void main(String[] args)
 	{
-		methode1();
+		ausgabe();
+		System.out.println("Die addition von a und b ist ="+addition(3, 4));
 	}
 	
-	public static void methode1()
+	public static void ausgabe()
 	{
 		System.out.println("HalloWelt");
 	}
+	
+	public static int addition(int a, int b)
+	{
+		return (a+b);
+	}
+	
 }
